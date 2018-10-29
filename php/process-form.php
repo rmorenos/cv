fd18010badb641aa42e88e77e33e5395c78ef3a5
RaulM <raulmose87@gmail.com>
2018-10-29 00:10:45 -0300

    Modificando correo

--- php/process-form.php
+++ php/process-form.php
@@ -6,7 +6,7 @@ if (isset($_REQUEST['name'],$_REQUEST['email'])) {
     $message = $_REQUEST['message'];
       
     // Set your email address where you want to receive emails. 
-    $to = 'youremail@mail.com';
+    $to = 'raul@neoservi.com';
       
     $subject = 'Contact Request From Website';
     $headers = "From: ".$name." <".$email."> \r\n";
